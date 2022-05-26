a1d0c1f32eac5d0dc384a00413ada4eaa4cfbfb3
Guilherme Petkovic <gjrpetkovic@gmail.com>
2022-05-26 18:53:28 -0300

    seed Movies with API

--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -2,6 +2,6 @@ class Bookmark < ApplicationRecord
   belongs_to :movie
   belongs_to :list
 
-  validates :movie, uniqueness: { scope: :list }
+  validates :movie, uniqueness: { scope: :list, message: 'already added' }
   validates :comment, length: { minimum: 6 }
 end
