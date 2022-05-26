e9c4481f8b4f9147b771cb5a543f9d0aab4af2f2
Guilherme Petkovic <gjrpetkovic@gmail.com>
2022-05-26 17:04:18 -0300

    defined create and new method for bookmarks

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -5,6 +5,17 @@ class BookmarksController < ApplicationController
   end
 
   def create
+    @bookmark = Bookmark.new(bookmark_params)
+    @list = List.find(params[:list_id])
+    @bookmark.list = @list
+    if @bookmark.save
+      redirect_to list_path(@bookmark.list)
+    else
+      render :new
+    end
+  end
 
+  def bookmark_params
+    params.require(:bookmark).permit(:comment, :movie_id)
   end
 end
